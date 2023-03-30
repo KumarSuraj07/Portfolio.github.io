a4730841b70b6484e292c58a97ac90ec4199005a
Suraj Kumar <111426350+KumarSuraj07@users.noreply.github.com>
2023-03-31 02:05:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+// function sendEmail() {
+// 	Email.send({
+// 	Host: "ksurajk2001@gmail.com",
+// 	Username : "<sender’s email address>",
+// 	Password : "<email password>",
+// 	To : '<recipient’s email address>',
+// 	From : "<sender’s email address>",
+// 	Subject : "<email subject>",
+// 	Body : "<email body>",
+// 	}).then(
+// 		message => alert("mail sent successfully")
+// 	);
+// }
